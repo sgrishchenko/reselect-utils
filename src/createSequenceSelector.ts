aa12f9931cd2576031a8d6cf8ea3d931161f71af
Alex Khizhniy <akhizhniy@spotware.com>
2019-07-16 11:17:38 +0300

    add custom selector creator in sequence selector

--- src/createSequenceSelector.ts
+++ src/createSequenceSelector.ts
@@ -3,17 +3,18 @@ import { ParametricSelector, Selector } from './types';
 
 function createSequenceSelector<S, R>(
   selectors: Selector<S, R>[],
+  customSelectorCreator?: typeof createSelector,
 ): Selector<S, R[]>;
 
 function createSequenceSelector<S, P, R>(
   selectors: ParametricSelector<S, P, R>[],
+  customSelectorCreator?: typeof createSelector,
 ): ParametricSelector<S, P, R[]>;
 
-function createSequenceSelector(selectors: any) {
-  return createSelector(
-    selectors,
-    (...results) => results,
-  );
+function createSequenceSelector(selectors: any, customSelectorCreator: any) {
+  const selectorCreator = customSelectorCreator || createSelector;
+
+  return selectorCreator(selectors, (...results: any) => results);
 }
 
 export default createSequenceSelector;

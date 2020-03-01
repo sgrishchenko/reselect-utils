1e5bbe2ccd027b7540c62683f26dca1ec48a5d5c
Sergey Grishchenko <s.i.grishchenko@gmail.com>
2020-03-01 23:43:36 +0200

    static site resource configuring

--- .storybook/webpack.config.ts
+++ .storybook/webpack.config.ts
@@ -1,12 +1,12 @@
 import { Configuration } from 'webpack';
 
 export default async ({ config }: { config: Configuration }) => {
-  config.module!.rules.push({
+  config.module?.rules.push({
     test: /\.tsx?$/,
     loader: 'ts-loader',
   });
 
-  config.resolve!.extensions!.push('.ts', '.tsx');
+  config.resolve?.extensions?.push('.ts', '.tsx');
 
   return config;
 };
